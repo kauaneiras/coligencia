cc72137131684e88aeaeafce582052c4d35280a4
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2024-01-04 11:42:37 -0300

    Feat: SobreMim

--- src/pages/1SobreMim.jsx
+++ src/pages/1SobreMim.jsx
@@ -1,33 +1,67 @@
 import React, {useState} from "react";
 import Navbar from "../components/NavBar";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Tilt } from 'react-tilt'
 import kauan from "../assets/images/kauan.png";
 
+
 export default function SobreMim(){
+  const navigate = useNavigate();
   const [color, setColor] = useState(localStorage.getItem('color') || '000000');
-  console.log(color);
+
+  const NavigateTo = (path) => {
+    navigate(path);
+  }
 
   return (
     <Container color={color}>
         <Navbar />
         <About>
-        <Tilt options={defaultOptions} style={{ height: 300, width: 200 }}>
-        <FotoKauan src={kauan} />
-        </Tilt>
-        <Tilt options={defaultOptions} style={{ height: 200, width: 550 }}>
-        <Text color={color}>
-          <h1>Olá, eu sou o Kauan</h1>
-          <p>
-            Sou desenvolvedor Full Stack e estudante de Engenharia de Software, apaixonado por tecnologia, 
-            em busca de aprender coisas novas e me aperfeiçoar.
-          </p>
-        </Text>
-        </Tilt>
-
+          {window.innerWidth > 768 ? <SobreMimDekstop color={color}/>  : <SobreMimMobile color={color}/>}
         </About>
+        <Tilt options={defaultOptions} style={{ height: 200, width: 300 }}>
+        <ButtonHabilidades color={color} onClick={() => NavigateTo('/habilidades')}>Habilidades</ButtonHabilidades>
+        </Tilt>
     </Container>
   );
+}
+
+function SobreMimDekstop({color}){
+  return (
+    <>
+    <Tilt options={defaultOptions} style={{ height: 350, width: 250 }}>
+    <FotoKauan src={kauan} color={color}/>
+    </Tilt>
+    <Space/>
+    <Tilt options={defaultOptions} style={{ height: 350, width: 550 }}>
+    <Text color={color}>
+      <h1>Olá, eu sou o Kauan</h1>
+      <p>
+        Desenvolvedor Full Stack e estudante de Engenharia de Software, apaixonado por tecnologia, 
+        em busca de aprender coisas novas e me aperfeiçoar.
+      </p>
+    </Text>
+    </Tilt>
+    </>
+  )
+}
+
+function SobreMimMobile({color}){
+  return (
+    <>
+    <Tilt options={defaultOptions}>
+    <FotoKauan src={kauan} color={color}/>
+    </Tilt>
+    <TextMobile color={color}>
+      <h1>Olá, eu sou o Kauan</h1>
+      <p>
+        Desenvolvedor Full Stack, estudante de Engenharia de Software e apaixonado por tecnologia.
+        Estou em busca de aprender coisas novas e me aperfeiçoar.
+      </p>
+    </TextMobile>
+    </>
+  )
 }
 
 const defaultOptions = {
@@ -48,6 +82,7 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     background-color: black;
+    flex-direction: column;
     padding: 5%;
     border: 5px solid #${props => props.color};
     overflow: hidden;
@@ -56,15 +91,21 @@ const Container = styled.div`
 const FotoKauan = styled.img`
   box-shadow: 0 20px 27px rgba(0, 0, 0, 0.05);
   transform-style: preserve-3d;
-  margin: 50px auto;
   width: 200px;
   height: 300px;
   object-fit: cover;
   object-position: center;
+  background-color: #${props => props.color};
+  border: 5px solid #${props => props.color};
   
   img {
     width: 100%;
     height: 100%;
+  }
+
+  @media (max-width: 768px) {
+    margin-top: 50%;
+    
   }
 `;
 
@@ -75,30 +116,88 @@ const About = styled.div`
   justify-content: center;
   @media (max-width: 768px) {
     flex-direction: column;
+    width: 400px;
   }
 `;
 
 const Text = styled.div`
+  border: 5px solid #${props => props.color};
+  padding: 5%;
+  display: flex;
+  height: 300px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  max-width: 500px;
+  text-align: justify;
+  magin-top: 5%;
+  h1 {
+    font-size: 2em;
+    font-weight: 1000;
+    font-family: 'Roboto', sans-serif;
+    color: #${props => props.color};
+    margin-bottom: 5%;
+  }
+  p {
+    font-size: 1.5em;
+    font-weight: 400;
+    font-family: 'Roboto', sans-serif;
+    color: #ffffff;
+    text-align: justify;
+  }
+`;
+
+const TextMobile = styled.div`
   padding: 5%;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  margin-right: 5%;
   max-width: 500px;
   text-align: justify;
+  magin-top: 10%;
   h1 {
-    font-size: 3em;
+    font-size: 1.8em;
     font-weight: 1000;
     font-family: 'Roboto', sans-serif;
     color: #${props => props.color};
     margin-bottom: 10%;
+    align-self: center;
   }
   p {
-    font-size: 1.5em;
-    font-weight: 600;
+    width: 80%;
+    align-self: center;
+    font-size: 1.4em;
+    font-weight: 400;
     font-family: 'Roboto', sans-serif;
-    color: #${props => props.color};
+    color: #ffffff;
     text-align: justify;
   }
+`;
+
+const ButtonHabilidades = styled.button`
+  height: auto;
+  width: 10em;
+  font-size: 2em;
+  font-weight: 600;
+  margin-top: 4em;
+  font-family: 'Roboto', sans-serif;
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+  color: #${props => props.color};
+  border: 2px solid #${props => props.color};
+
+  &:hover {
+    background-color: #${props => props.color};
+    color: #000;
+  }
+
+  @media (max-width: 768px) {
+    margin-top: 1em;
+  }
+`;
+
+const Space = styled.div`
+  width: 10%;
 `;
